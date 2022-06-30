481554541ceae0cf5cd559163700418a54922624
kasyap-007 <rishikasyap007@gmail.com>
2022-06-30 22:07:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-7cdeym
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-7cdeym)
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+      <p className="text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-sky-500 via-purple-500 to-pink-600">
+        Next Tailwind
+      </p>
+    </div>
+  );
+}
